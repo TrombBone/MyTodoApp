dcca1f4cd2a47e9da380d52455280f0bd85cd2f7
Konstantin <kruchinin-kostya@mail.ru>
2024-05-20 12:07:14 +0300

    Added resetting of alarms after changing the system time

---
+++ app/src/main/java/com/example/mytodoapp/features/notifications/receivers/SystemTimeChangedReceiver.kt
@@ -0,0 +1,32 @@
+package com.example.mytodoapp.features.notifications.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.example.mytodoapp.features.database.repositories.TaskRepository
+import com.example.mytodoapp.features.notifications.NotificationAlarmManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class SystemTimeChangedReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var taskRepository: TaskRepository
+
+    @Inject
+    lateinit var notificationAlarmManager: NotificationAlarmManager
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == "android.intent.action.TIME_SET") {
+            CoroutineScope(Dispatchers.IO + NonCancellable).launch {
+                taskRepository.allTasks.collect { tasks ->
+                    tasks.forEach { task -> notificationAlarmManager.setAlarmInFuture(task) }
+                }
+            }
+        }
+    }
+
+}
